172f948f53b448bd64b5d4a8a1282938b8a32028
“Daniel” <dev@dev.com>
2017-01-02 17:01:39 -0500

    Initial Commit

---
+++ Swiftium/Swiftium.h
@@ -0,0 +1,19 @@
+//
+//  Swiftium.h
+//  Swiftium
+//
+//  Created by dman on 1/2/17.
+//  Copyright © 2017 Daniel Hong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swiftium.
+FOUNDATION_EXPORT double SwiftiumVersionNumber;
+
+//! Project version string for Swiftium.
+FOUNDATION_EXPORT const unsigned char SwiftiumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftium/PublicHeader.h>
+
+
