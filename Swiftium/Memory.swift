c0b263a9756e239c92b84063aefb7a64991f2e2a
“Daniel” <dev@dev.com>
2017-01-02 17:16:36 -0500

    initial commit

---
+++ Swiftium/Memory.swift
@@ -0,0 +1,35 @@
+//
+//  Memory.swift
+//  Swiftium
+//
+//  Created by dman on 1/2/17.
+//  Copyright © 2017 Daniel Hong. All rights reserved.
+//
+
+import Foundation
+
+public class Memory {
+    
+    //
+    // Secure zero memory
+    //
+    // - Parameter data: NSMutableData to zero
+    //
+    public func zero(data: NSMutableData) {
+        // Clear the memory
+        sodium_memzero(data.mutableBytesPtr(), data.length)
+    }
+    
+    //
+    // Checks if the given data contains all zeros
+    //
+    // - Parameter data: The data to check
+    //
+    // - Returns: True if the data contains all zeros, false otherwise
+    //
+    public func isZero(data: NSData) -> Bool {
+        let res = sodium_is_zero(data.bytesPtr(), data.length)
+        return res == 1
+    }
+    
+}
---
+++ Swiftium/NSData+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  NSData+Extensions.swift
+//  Swiftium
+//
+//  Created by dman on 1/2/17.
+//  Copyright © 2017 Daniel Hong. All rights reserved.
+//
+
+import Foundation
+
+public extension NSData {
+    func bytesPtr<T>() -> UnsafePointer<T>{
+        let rawBytes = self.bytes
+        return rawBytes.assumingMemoryBound(to: T.self);
+    }
+}
+
+public extension NSMutableData {
+    func mutableBytesPtr<T>() -> UnsafeMutablePointer<T>{
+        let rawBytes = self.mutableBytes
+        return rawBytes.assumingMemoryBound(to: T.self)
+    }
+}
